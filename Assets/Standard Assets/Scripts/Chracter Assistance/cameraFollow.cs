b55bee40b8e9c703765477ece9c9c1753f2e6b38
blavek <blavek@comcast.net>
2016-10-10 15:39:49 -0400

    Duel
    Test Scene and character controller.

---
+++ Assets/Standard Assets/Scripts/Chracter Assistance/cameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraFollow : MonoBehaviour {
+	public GameObject target;
+	public int camHeightOffset = 1;
+	public int distanceOffset = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 tar = target.transform.position;
+		transform.position = new Vector3 (tar.x, tar.y + camHeightOffset, tar.z - distanceOffset);
+		transform.LookAt (tar);
+
+	}
+}
