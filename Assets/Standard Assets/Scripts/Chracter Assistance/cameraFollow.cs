7d747fc592b51d61a3204ea70873d302643b73fd
blavek <blavek@comcast.net>
2016-10-17 15:52:05 -0400

    Character Controller Updates

--- Assets/Standard Assets/Scripts/Chracter Assistance/cameraFollow.cs
+++ Assets/Standard Assets/Scripts/Chracter Assistance/cameraFollow.cs
@@ -3,19 +3,21 @@ using System.Collections;
 
 public class cameraFollow : MonoBehaviour {
 	public GameObject target;
-	public int camHeightOffset = 1;
+	private int camHeightOffset = 5;
 	public int distanceOffset = 10;
 
 	// Use this for initialization
 	void Start () {
-	
+		Vector3 tar = target.transform.position;
+		transform.position = new Vector3 (tar.x, tar.y + camHeightOffset, tar.z - distanceOffset);
+		transform.LookAt (tar);	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Vector3 tar = target.transform.position;
 		transform.position = new Vector3 (tar.x, tar.y + camHeightOffset, tar.z - distanceOffset);
-		transform.LookAt (tar);
+//		transform.LookAt (tar);
 
 	}
 }
