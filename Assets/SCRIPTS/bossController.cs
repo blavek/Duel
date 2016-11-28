4af3373f121046fbb6c766a89bf92475bf593378
Tbream21 <trevorbream@gmail.com>
2016-11-27 21:51:46 -0500

    adding enemies and boss to levels
    the levels are more populated then they were before and the boss has a
    script that will need some work(it is the same as the enemy script).

---
+++ Assets/SCRIPTS/bossController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bossController : MonoBehaviour {
+	public float speed = 5f;
+	public GameObject pc;
+	public float chaseDist = 25f;
+	public float attackDist = 2f;
+	public int startingHp = 20;
+	public GameObject self;
+	int HP;
+
+	Animator anim;
+
+	Vector3 relVec;
+	float attackLen = .7f;
+	float attacking = 0;
+
+	// Use this for initialization
+	void Start () {
+		relVec = pc.transform.position - transform.position;
+		anim = GetComponent<Animator> ();
+		HP = startingHp;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		relVec = pc.transform.position - transform.position;
+		if (attacking <= 0) {
+			rotate ();
+			move ();
+			attack ();
+		} else {
+			attacking -= Time.deltaTime;
+		}
+	}
+
+	void rotate() {
+		if (relVec.magnitude <= chaseDist) {
+			float rot = Mathf.Atan2 (relVec.x, relVec.z) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.Euler (new Vector3 (0, rot, 0));
+		}
+	}
+
+	void move() {
+		if (relVec.magnitude <= chaseDist && relVec.magnitude > attackDist) {
+			float translation = speed * Time.deltaTime;
+			transform.Translate (new Vector3 (0, 0, translation));
+			anim.SetBool ("Idle", false);
+			anim.SetFloat ("Move", translation);
+		} else if (relVec.magnitude > chaseDist && relVec.magnitude > attackDist) {
+			anim.SetBool ("Idle", true);
+			anim.SetFloat ("Move", 0);
+		} else {
+			anim.SetFloat ("Move", 0);
+		}
+	}
+
+	void attack() {
+		if (relVec.magnitude <= attackDist) {
+			anim.SetBool ("Idle", false);
+			anim.SetBool ("Attack", true);
+			attacking = attackLen;
+			Invoke ("stopAttack", attackLen);
+		}
+	}
+
+	void stopAttack() {
+		anim.SetBool ("Attack", false);
+		anim.SetBool ("Idle", true);
+	}
+
+	public void damage (int dmg) {
+		HP -= dmg;
+
+		if (HP <= 0) {
+			Instantiate (self, new Vector3 (0, 0, 0), Quaternion.identity);
+			Destroy (gameObject);
+		}
+	}
+}
