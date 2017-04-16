834c43afac5928463988e2f40f3bbac392475030
demipotato <bryhendrick@yahoo.com>
2017-04-15 20:18:11 -0400

    Models Ready - DragonBoss + FlyingEnemy
    The dragon boss and the bird enemy are textured and have animations and
    are ready to be used. All they need is code and they will be ready to
    go.

--- Assets/SCRIPTS/Controller.cs
+++ Assets/SCRIPTS/Controller.cs
@@ -9,10 +9,12 @@ public class Controller : MonoBehaviour {
 	public float rotationSpeed = 100.0F;
     public float jumpForce = 100f;
     public Reticle reticle;
+	public float attackDistance = 2;
 
     private GameObject lockOnTarget = null;
     private bool lockedOn = false;
     private Rigidbody rBody;
+	private GameObject cam = null;
 //    private bool grounded = true;
 
 	float attackLen = .467f;
@@ -21,12 +23,17 @@ public class Controller : MonoBehaviour {
 	void Start () {
 		anim = GetComponent<Animator> ();
         rBody = GetComponent<Rigidbody> ();
+		cam = GameObject.FindGameObjectWithTag ("MainCamera");
 		Debug.Log (anim.name /*["Attack"].length*/);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (!lockedOn)
+		if (lockOnTarget == null) {
+			lockOff ();
+		}
+
+		if (!lockedOn)
             moveNoTarget ();
         else
             moveTarget ();
@@ -46,6 +53,7 @@ public class Controller : MonoBehaviour {
 		}
             
         if (Input.GetButtonDown ("Jump")) {
+			Debug.Log ("JUMP");
             jump ();
         }
 
@@ -68,9 +76,18 @@ public class Controller : MonoBehaviour {
 //			y = 0;
 
 		float translation = speed * (Mathf.Sqrt((x*x) + (y*y)));
-        float rot = Mathf.Atan2 (x, y) * Mathf.Rad2Deg;
+		float joyRot = ((Mathf.Atan2 (x, y) * Mathf.Rad2Deg) + 360) % 360;
+		float camRot = cam.transform.eulerAngles.y;
+		float rot = (joyRot + camRot) % 360;
 
-//        Debug.Log (rot + " x:" + x + " y:" + y);
+//		Vector3 rot = y * cam.transform.forward + x * cam.transform.right;
+
+
+//		rot = Mathf.LerpAngle (transform.eulerAngles.y, rot, rotationSpeed * Time.deltaTime);
+//		rot = Camera.main.transform.TransformDirection(new Vector3 (0, rot, 0));
+
+//        Debug.Log ("CamRot: " + camRot + "JoyRot: " + joyRot + " x:" + x + " y:" + y);
+
 		translation *= Time.deltaTime;
 		transform.Translate(new Vector3(0, 0, translation));
 
@@ -83,6 +100,21 @@ public class Controller : MonoBehaviour {
     void moveTarget() {
         float lx = Input.GetAxis ("Horizontal");
         float ly = Input.GetAxis ("Vertical");
+		transform.LookAt (lockOnTarget.transform);
+//		transform.rotation = new Quaternion (0, transform.rotation.y, 0, 0);
+
+		float yTranslation = speed * ly * Time.deltaTime;
+
+		if (Vector3.Distance (transform.position, lockOnTarget.transform.position) < attackDistance && yTranslation > 0)
+			yTranslation = 0;
+		
+		float xTranslation = speed * lx * Time.deltaTime;
+
+        transform.Translate(new Vector3(xTranslation, 0, yTranslation));
+
+		float translation = xTranslation > yTranslation ? xTranslation : yTranslation;
+
+        animate (translation);
     }
 
     void animate (float translation) {
@@ -128,7 +160,7 @@ public class Controller : MonoBehaviour {
 
     void lockOn() {
         lockedOn = true;
-        Debug.Log ("LOCKON");
+ //       Debug.Log ("LOCKON");
         GameObject [] targets = GameObject.FindGameObjectsWithTag("Enemy");
 
         if (targets.Length == 0)
