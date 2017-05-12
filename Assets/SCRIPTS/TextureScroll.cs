43766589eb54fa812f007dde285dcc2bf95ce69a
Tbream21 <trevorbream@gmail.com>
2017-05-12 16:01:10 -0400

    Changes 3.0
    getting there

---
+++ Assets/SCRIPTS/TextureScroll.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureScroll : MonoBehaviour {
+
+	public float ScrollX = 0.5f;
+	public float ScrollY = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float OffsetX = Time.time * ScrollX;
+		float OffsetY = Time.time * ScrollY;
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+	}
+}
